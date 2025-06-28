5ded3a0d443eb6a3fdbc713b3b3215001743c455
Devran Mete <devranm600gmail.com>
2025-06-28 23:19:29 +0300

    day_5

---
+++ day_5.py
@@ -0,0 +1,18 @@
+#Çift-Tek Sayı Bulucu
+#kullanıcınınn girdiği sayının çift mi tek mi olduğunu bulan program
+
+
+# 1-Kullanıcıdan sayı al:
+sayi =int(input("Bir sayı giriniz: "))
+
+# 2-Çift mi diye kontrol et:
+if sayi % 2 == 0:
+    # 3-Çiftse ekrana yaz:
+    print(f"{sayi} sayısı çifttir.")
+    # 4-Aksi halde tektir:
+else:
+    print(f"{sayi} sayısı tektir.")
+
+
+#?Cevapları (Yorumlarda  belirtiniz)
+#*int  ne  için   kullanılır?
